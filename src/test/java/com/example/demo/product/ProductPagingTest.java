439d1536df7001427b6d0144547cf290520551d9
sungchan <awdr1057@naver.com>
2025-07-19 16:10:40 +0900

    fix: 테스트 코드 오류 해결

--- src/test/java/com/example/demo/product/ProductPagingTest.java
+++ src/test/java/com/example/demo/product/ProductPagingTest.java
@@ -9,12 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class ProductPagingTest {
 
   @Autowired
